03c400c9647c3bd3a248ac46a5c3174f82691c7a
regis471 <regimantasignatjevas@gmail.com>
2021-01-19 09:40:35 +0200

    NumCount

--- js/numCount.js
+++ js/numCount.js
@@ -1,4 +1,26 @@
 /*
 
 0 - 0
+
 */
+
+
+const from = -70;
+const till = -30;
+let sum=0;
+
+
+for (let i=0;  i <5;  i++){
+    sum +=i;
+    console.log(i, sum);
+}
+console.log('==================');
+ let suma=0;
+
+for (let i=from;  i <till;  i++){
+    suma +=i;
+    
+}
+
+const ats = `Suma nuo ${from} iki ${till} yra ${suma}.`;
+console.log(ats);
