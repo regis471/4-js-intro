99b422c245855c4247b62f23a876a7e16943cd15
regis471 <regimantasignatjevas@gmail.com>
2021-01-19 11:32:51 +0200

    reverse js

--- js/numCount.js
+++ js/numCount.js
@@ -1,26 +1,42 @@
 /*
 
 0 - 0
-
+numcount - kai reikia sumuoti skaicius intervale
 */
 
 
-const from = -70;
-const till = -30;
+const from = 0;
+const till = 5;
 let sum=0;
 
 
-for (let i=0;  i <5;  i++){
+for (let i=0;  i <=5;  i++){
     sum +=i;
     console.log(i, sum);
 }
 console.log('==================');
  let suma=0;
 
-for (let i=from;  i <till;  i++){
+for (let i=from;  i <=till;  i++){
     suma +=i;
     
 }
 
 const ats = `Suma nuo ${from} iki ${till} yra ${suma}.`;
 console.log(ats);
+
+
+/*
+
+i = 3; 3 <= 5 -> true
+    sum = 0 + 3 = 3
+    i++ -> i=4 
+i = 4; 4 <= 5 -> true
+    sum = 3 + 4 = 7
+    i++ -> i=5 
+i = 5; 5 <=5 -> true
+    sum = 7 + 5 = 12
+    i++ -> i=6
+i = 6; 6 <= -> false
+
+*/
---
+++ js/reverse.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/*
+Perasyti teksta is galo i prieki.
+*/
+
+const text = 'abcdef';
+const size = text.length;
+let reverseText = '';
+
+// for (let i = 0; i < size; i++){
+//     const letter = text[i];
+//     reverseText = letter + reverseText;
+//     console.log(i, letter, reverseText);
+// }
+
+// console.log('Finale gavau:', reverseText);
+
+
+// for (let i = 0; i < size; i++){
+//     const letter = text[size - i - 1];
+//     reverseText += letter;
+//     console.log(i, letter, reverseText);
+// }
+
+
+
+
+// for (let i = 1; i <= size; i++){
+//     const letter = text[size - i ];
+//     reverseText += letter;
+//     console.log(i, letter, reverseText);
+// }
+
+
+for (let i = size - 1; i >= 0; i--) {
+    const letter = text[i];
+    reverseText += letter;
+    console.log(i, letter, reverseText);
+}
+console.log('Finale gavau:', reverseText);
+
