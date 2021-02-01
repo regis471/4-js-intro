5781e95cd7fe03d78dfb5b8f6f7c2c9714efa7cd
regis471 <76951483+regis471@users.noreply.github.com>
2021-02-01 08:32:07 +0200

    Daugybos pabaiga

--- js/multiply.js
+++ js/multiply.js
@@ -5,13 +5,40 @@ Salygos:
 */
 function multiply(a, b){
     if (typeof a !== 'number'){
-        return 'ERROR: pirmasis parametras nera skaicius';
+        return 'ERROR: pirmasis parametras nera skaicius';        
     }
+    if ('' + a === 'NaN'){
+        return 'ERROR: pirmasis parametras turi buti tikras skaicius';        
+    }
+    // alternatyvus uzrasymas
+    // if (isNaN(a)){
+    //     return 'ERROR: pirmasis parametras turi buti tikras skaicius';        
+    // }
+
+
+    if ('' + a === Infinity || a=== -Infinity){
+        return 'ERROR: pirmasis parametras negali buti begalybe.';        
+    }
+    // alternatyvus uzrasymas
+    // if (Math.abs(a) === Infinity){
+    //     return 'ERROR: pirmasis parametras negali buti begalybe.';        
+    // }
+
+
     if (typeof b !== 'number'){
         return 'ERROR: antrasis parametras nera skaicius';
     }
+    // if (isNan(b)){
+    //     return 'ERROR: antrasis parametras tikras skaicius';
+    // }
+    if (!isFinite(b)){
+        return 'ERROR: antrasis parametras tikras skaicius';
+    }
 
-    return a * b ;
+    const rez = a * b;
+
+    return rez === 0 ? 0 : rez;
+    // return a * b === 0 ? 0 : a * b; pavydys kaip yra parasyta salyga
 }
 
 const m1 = multiply(2, 2);
@@ -26,12 +53,6 @@ const m3 = multiply(-4, 8.5);
 console.log(m3);
 
 
-console.log(multiply(5, 8), '->', 40);
-console.log(multiply(-5, 8), '->', -40);
-console.log(multiply(-5, -8), '->', 40);
-console.log(multiply(0.5, 8), '->', 4);
-console.log(multiply(0.5, 2.6), '->', 1.3);
-console.log(multiply(-1, 0), '->', -0);
 
 
 console.log(multiply('labas', 'rytas'));
@@ -48,7 +69,21 @@ console.log(multiply(NaN, 0));
 console.log(multiply(NaN, NaN));
 console.log(multiply(8  , NaN));
 
+console.log(multiply(Infinity, 0));
+console.log(multiply(Infinity, Infinity));
+console.log(multiply(8  , Infinity));
 
+console.log(multiply(-Infinity, 0));
+console.log(multiply(-Infinity, -Infinity));
+console.log(multiply(8  , -Infinity));
+
+
+console.log(multiply(5, 8), '->', 40);
+console.log(multiply(-5, 8), '->', -40);
+console.log(multiply(-5, -8), '->', 40);
+console.log(multiply(0.5, 8), '->', 4);
+console.log(multiply(0.5, 2.6), '->', 1.3);
+console.log(multiply(-1, 0), '->', -0);
 
 
 
